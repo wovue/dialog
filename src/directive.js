6bd03c16efec9ba7f631e4b27d90ebdd4caf6c51
wochap <gean.marroquin@gmail.com>
2016-09-18 18:53:58 -0500

    fix posible memory leak

--- src/directive.js
+++ src/directive.js
@@ -11,12 +11,17 @@ const directive = {
         console.error(`[dialog]: Expected String, got ${typeof this.expression}. (found in ${this.vm.constructor.name})`)
       }
 
-      this.el.addEventListener('click', toggleDialog.bind(undefined, this.expression))
+      // bindings
+      // http://stackoverflow.com/questions/14417890/does-bind-change-the-function-reference-how-to-set-permanently?lq=1
+      this.$wv = {}
+      this.$wv.toggleDialog = toggleDialog.bind(undefined, this.expression)
+
+      this.el.addEventListener('click', this.$wv.toggleDialog)
     }
   },
   unbind () {
     if (this.arg === 'toggle') {
-      this.el.removeEventListener('click', toggleDialog.bind(undefined, this.expression))
+      this.el.removeEventListener('click', this.$wv.toggleDialog)
     }
   }
 }
