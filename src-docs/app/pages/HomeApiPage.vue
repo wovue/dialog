133b644d47de4733faea0d49cb8edf53833dc903
wochap <gean.marroquin@gmail.com>
2016-09-18 13:20:17 -0500

    update api page

--- src-docs/app/pages/HomeApiPage.vue
+++ src-docs/app/pages/HomeApiPage.vue
@@ -5,6 +5,11 @@
       <h4 class="c-item__heading">Dialog</h4>
       <h5 class="c-item__sub-heading">Props</h5>
       <app-table class="c-item__table" :data="tables.props"></app-table>
+      <h5 class="c-item__sub-heading">Events</h5>
+      <app-table class="c-item__table" :data="tables.events"></app-table>
+      <h4 class="c-item__heading">Directives</h4>
+      <h5 class="c-item__sub-heading">v-dialog</h5>
+      <app-table class="c-item__table" :data="tables.directive"></app-table>
     </div>
   </section>
 </template>
@@ -15,12 +20,75 @@
     data () {
       return {
         tables: {
+          directive: [
+            {
+              expression: 'Dialog identifier. (the `ref` prop from the Dialog)',
+              expects: 'String',
+              argument: 'toggle',
+              description: 'Toggle the Dialog on click.',
+              example: '`<button v-dialog:toggle="stuff">Open</button>`'
+            }
+          ],
           props: [
             {
-              name: 'modifier',
-              type: '`string`',
+              name: 'ref',
+              type: '`String`',
               required: 'true',
-              default: '`""`'
+              default: null,
+              description: 'Dialog identifier.'
+            }, {
+              name: 'overlayBackground',
+              type: '`String`',
+              required: 'false',
+              default: '`rgba(0, 0, 0, .5)`',
+              description: 'Overlay background color.'
+            }, {
+              name: 'class',
+              type: '`String`',
+              required: 'false',
+              default: '`false`',
+              description: 'Dialog `class` attribute.'
+            }, {
+              name: 'width',
+              type: '`Number`, `String`',
+              required: 'false',
+              default: '`false`',
+              description: 'Dialog `max-width` style rule. (e.g. `300`, `70%`)'
+            }, {
+              name: 'closeOnEsc',
+              type: '`Boolean`',
+              required: 'false',
+              default: '`true`',
+              description: 'This determines if pressing the `esc` key will close the modal.'
+            }, {
+              name: 'closeOnOutsideClick',
+              type: '`Boolean`',
+              required: 'false',
+              default: '`true`',
+              description: 'This determines if clicking on the overlay will close the modal.'
+            }, {
+              name: 'ariaLabelledby',
+              type: '`String`',
+              required: 'false',
+              default: '`false`',
+              description: 'Dialog `aria-labelledby` attribute.'
+            }, {
+              name: 'ariaDescribedby',
+              type: '`String`',
+              required: 'false',
+              default: '`false`',
+              description: 'Dialog `aria-describedby` attribute.'
+            }
+          ],
+          events: [
+            {
+              name: 'opened',
+              type: 'emitted',
+              description: 'Fired when the Dialog is opened. The transition starts.'
+            }, {
+              name: 'closed',
+              type: 'emitted',
+              description: 'Fired when the Dialog is closed. The transition ends.'
             }
           ]
         }
