ab02f596087b675f1585c1e17a5f723b021184f6
wochap <gean.marroquin@gmail.com>
2016-09-19 20:36:47 -0500

    update Installation page

--- src-docs/app/pages/HomeDefaultPage.vue
+++ src-docs/app/pages/HomeDefaultPage.vue
@@ -2,20 +2,21 @@
   <section class="c-main__sub-section">
     <h3 class="c-main__sub-heading u-gutter">Installation</h3>
     <div class="c-item u-gutter">
+      <div class="c-item__description">
+        <p>You have two ways to setup <code>wovue-dialog</code>:</p>
+      </div>
+      <h4 class="c-item__heading">NPM</h4>
       <div class="c-item__code-wrapper" v-html="snippets.installation"></div>
-      <div class="c-item__description">
-        <p>You have two ways to setup dialog:</p>
-      </div>
-      <h5 class="c-item__sub-heading">Include</h5>
-      <div class="c-item__description">
-        <p>Include it directly with a <code>&lt;script&gt;</code> tag. In this case, you don't need to write <code>Vue.use(Dialog)</code>, this will be done automatically for you.</p>
-      </div>
-      <div class="c-item__code-wrapper" v-html="snippets.installationInclude"></div>
       <h5 class="c-item__sub-heading">CommonJS (Webpack/Browserify)</h5>
       <div class="c-item__description">
         <p>If you are working with <code>Browserify</code>, you will have to add the css separately.</p>
       </div>
       <div class="c-item__code-wrapper" v-html="snippets.installationCommonjs"></div>
+      <h4 class="c-item__heading">Direct include (Browser)</h4>
+      <div class="c-item__description">
+        <p>Include it directly with a <code>&lt;script&gt;</code> tag. In this case, you don't need to write <code>Vue.use(Dialog)</code>, this will be done automatically for you.</p>
+      </div>
+      <div class="c-item__code-wrapper" v-html="snippets.installationInclude"></div>
     </div>
   </section>
 </template>
--- src-docs/app/snippets/installation-include.md
+++ src-docs/app/snippets/installation-include.md
@@ -3,12 +3,12 @@
 <html>
   <head>
     ...
-    <link rel="stylesheet" href="path/to/dialog.css"/>
+    <link rel="stylesheet" href="https://unpkg.com/wovue-dialog/dist/dialog.css"/>
     ...
   </head>
   <body>
     <script src="path/to/vue.js"></script>
-    <script src="path/to/dialog.js"></script>
+    <script src="https://unpkg.com/wovue-dialog/dist/dialog.js"></script>
     <script src="path/to/app.js"></script>
   </body>
 </html>
