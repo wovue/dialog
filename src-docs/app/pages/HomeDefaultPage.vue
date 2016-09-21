bbf0c4958c837092121bbc0436a2a095aa171ea8
wochap <gean.marroquin@gmail.com>
2016-09-20 21:35:55 -0500

    chore(Installation):
    wrap the css word with the code tag

--- src-docs/app/pages/HomeDefaultPage.vue
+++ src-docs/app/pages/HomeDefaultPage.vue
@@ -9,7 +9,7 @@
       <div class="c-item__code-wrapper" v-html="snippets.installation"></div>
       <h5 class="c-item__sub-heading">CommonJS (Webpack/Browserify)</h5>
       <div class="c-item__description">
-        <p>If you are working with <code>Browserify</code>, you will have to add the css separately.</p>
+        <p>If you are working with <code>Browserify</code>, you will have to add the <code>css</code> separately.</p>
       </div>
       <div class="c-item__code-wrapper" v-html="snippets.installationCommonjs"></div>
       <div class="c-item__description">
