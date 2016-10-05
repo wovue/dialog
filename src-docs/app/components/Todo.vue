c1c32850b48dadb45d5ac964d02c4500c4930785
wochap <gean.marroquin@gmail.com>
2016-10-05 16:24:46 -0500

    docs(Examples): add a todo component in the dialog example

---
+++ src-docs/app/components/Todo.vue
@@ -0,0 +1,64 @@
+<template>
+  <section class="todo">
+    <h1>Todo List</h1>
+    <form class="todo__form" @submit.prevent="add">
+      <input v-model="newTodo" type="text" placeholder="new todo"/>
+      <button class="todo__btn" type="submit">Add</button>
+    </form>
+
+    <ul class="todo__list">
+      <li v-for="todo in todos">{{ todo.text }} <button class="todo__btn" @click="remove(todo)" type="button">Delete</button></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      todos: [],
+      newTodo: ''
+    }
+  },
+  methods: {
+    add () {
+      if (!this.newTodo) return
+
+      this.todos.push({
+        text: this.newTodo
+      })
+
+      this.newTodo = ''
+    },
+    remove (todo) {
+      if (!todo) return
+
+      this.todos.splice(this.todos.indexOf(todo), 1)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.todo {
+  margin: 1rem 0;
+  background-color: #ffffff;
+  color: #000000;
+  padding: 2rem;
+}
+
+  .todo__form {
+    input {
+      border: 1px solid #000000;
+    }
+  }
+
+  .todo__list {
+    list-style: none;
+    text-align: left;
+  }
+
+  .todo__btn {
+
+  }
+</style>
--- src-docs/app/config/registerComponents.js
+++ src-docs/app/config/registerComponents.js
@@ -1,7 +1,9 @@
 import AppFooter from 'components/AppFooter'
 import AppTable from 'components/AppTable'
+import Todo from '../components/Todo'
 
 export default function registerComponents (Vue) {
   Vue.component('app-footer', AppFooter)
   Vue.component('app-table', AppTable)
+  Vue.component('todo', Todo)
 }
